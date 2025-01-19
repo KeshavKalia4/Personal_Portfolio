5257219baca5ae5fac3b47f27ed2f7ec008de737
Keshav Kalia <keshavkalia30@gmail.com>
2025-01-19 00:19:50 -0800

    Updates

---
+++ precache-manifest.d2b089ad21fc1aa5e0abddfa7a0fdc45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a16b82c36969fda609975af1fb2fdd0",
+    "url": "/Personal_Portfolio/index.html"
+  },
+  {
+    "revision": "f9bf0e952a5c75939760",
+    "url": "/Personal_Portfolio/static/css/2.71fe5322.chunk.css"
+  },
+  {
+    "revision": "0454003d9ef90b0a62d2",
+    "url": "/Personal_Portfolio/static/css/main.5205a517.chunk.css"
+  },
+  {
+    "revision": "f9bf0e952a5c75939760",
+    "url": "/Personal_Portfolio/static/js/2.dd03e582.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Personal_Portfolio/static/js/2.dd03e582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0454003d9ef90b0a62d2",
+    "url": "/Personal_Portfolio/static/js/main.1273d0f0.chunk.js"
+  },
+  {
+    "revision": "c1b32c22cbcc32bbe6e2",
+    "url": "/Personal_Portfolio/static/js/runtime-main.11a3645b.js"
+  }
+]);
